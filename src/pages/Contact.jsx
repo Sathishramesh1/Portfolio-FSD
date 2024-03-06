7f9eb9e7a385d5f4cdc3967a3a5c1b81c8521201
Sathishramesh1 <sathishrameshkec@gmail.com>
2024-03-06 17:23:49 +0530

    contact-page-update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,14 +6,20 @@ import { UseLottieAnimation } from '../components';
 import { contact } from '../assets';
 import { BsSendFill } from 'react-icons/bs';
 
+
+
+
+const serviceId = import.meta.env.VITE_APP_EMAILJS_SERVICE_ID;
+const templateId = import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID;
+const publicKey = import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY;
+
 const Contact = () => {
   const [loading, setLoading] = useState(false);
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
   const data = JSON.stringify(contact);
 
-  const serviceId = import.meta.env.VITE_APP_EMAILJS_SERVICE_ID;
-  const templateId = import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID;
-  const publicKey = import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY;
+  
+
 
   const onSubmit = (data) => {
     console.log(data);
@@ -23,9 +29,9 @@ const Contact = () => {
         templateId,
         {
           from_name: data.name,
-          to_name: "Maduakor Emmanuel",
+          to_name: "sathish",
           from_email: data.email,
-          to_email: "emma080355@gmail.com",
+          to_email: "sathishrameshkec@gmail.com",
           message: data.message,
         },
         publicKey
@@ -46,13 +52,13 @@ const Contact = () => {
 
   return (
     <div id='contact' className='app__page primary relative'>
-      <h1 className='mt-20 md:mt-10 text-dark-accent ml-7 '>
-        Let's <span className='text-blue-500'>Connect!</span>
+      <h1 className='mt-20 md:mt-10 text-green-500 ml-7 '>
+        Let's Connect!
       </h1>
 
       <div className='flex flex-col-reverse items-center justify-between mx-auto mt-10 mb-10 lg:w-4/5 lg:flex-row-reverse md:mb-20'>
         <form
-          className="w-4/5 mx-auto md:w-2/4 xl:w-2/6"
+          className="w-4/5 mx-auto md:w-2/4 xl:w-2/6 "
           onSubmit={handleSubmit(onSubmit)}
         >
           <div className="w-full mx-auto mb-6">
@@ -61,7 +67,7 @@ const Contact = () => {
             </label>
             <input
               {...register('name', { required: true })}
-              className='w-full px-1 py-2 rounded-md dark:bg-light-secondary focus:outline-none bg-light-accent text-dark-primary lg:py-4'
+              className='w-full px-1 py-2 rounded-md dark:bg-white focus:outline-none bg-white text-black lg:py-4'
             />
             {errors.name && (
               <span className="text-red-500">This field is required</span>
@@ -74,7 +80,7 @@ const Contact = () => {
             </label>
             <input
               {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
-              className='w-full px-1 py-2 rounded-md dark:bg-light-secondary focus:outline-none bg-light-accent text-dark-primary lg:py-4'
+              className='w-full px-1 py-2 rounded-md dark:bg-white focus:outline-none bg-white text-black lg:py-4'
             />
             {errors.email && (
               <span className="text-red-500">
@@ -89,7 +95,7 @@ const Contact = () => {
             </label>
             <textarea
               {...register('message', { required: true })}
-              className='w-full h-20 p-1 px-2 pt-2 rounded-md lg:h-24 dark:bg-light-secondary focus:outline-none bg-light-accent text-dark-primary'
+              className='w-full h-20 p-1 px-2 pt-2 rounded-md lg:h-24 dark:bg-white focus:outline-none bg-white text-black'
             ></textarea>
             {errors.message && (
               <span className="text-red-500">This field is required</span>
@@ -98,7 +104,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className='w-full p-2 px-5 rounded-md dark:bg-dark-accent dark:text-dark-primary/80 bg-light-secondary text-dark-primary lg:py-3 hover:dark:bg-light-accent flex justify-center items-center hover:bg-light-secondary/80'
+            className='w-full p-2 px-5 rounded-md dark:bg-green-500 dark:text-dark-primary/80 bg-green-500 text-dark-primary lg:py-3 hover:dark:bg-light-accent flex justify-center items-center hover:bg-light-secondary/80'
           >
             Send Message <BsSendFill className='ml-2' />
           </button>
